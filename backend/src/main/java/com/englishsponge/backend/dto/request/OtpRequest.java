7ce319ac31ba7a74db7a6652c7101d721dc228d1
Jamie Furlong <jamiefurlong16@gmail.com>
2026-01-28 13:51:12 +0000

    Phase 1: Remove backend and API test collections

--- backend/src/main/java/com/englishsponge/backend/dto/request/OtpRequest.java
+++
@@ -1,9 +0,0 @@
-package com.englishsponge.backend.dto.request;
-
-import lombok.Data;
-
-@Data
-public class OtpRequest {
-    private String email;
-    private Integer otp;
-}
--- backend/src/main/java/com/englishsponge/backend/exception/InvalidRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.englishsponge.backend.exception;
-
-public class InvalidRequestException extends RuntimeException {
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-}
